3996a8aa2ef48fe026f6ffde35b0970ccc43e80c
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-09-28 22:44:50 +0200

    Añadir test para get_files_with_text

--- tests/unit_tests/test_get_files_with_text.py
+++ tests/unit_tests/test_get_files_with_text.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from lib.files import get_files_with_text
+from utils.files import get_files_with_text
 
 
 @pytest.fixture
@@ -10,6 +10,7 @@ def init_data_folder(tmp_path: Path) -> Path:
     (tmp_path / "1.md").write_text("# Title\nSome text here.")
     (tmp_path / "2.md").write_text("Just some text.")
     (tmp_path / "3.md").write_text("Another markdown file.")
+    (tmp_path / "4.txt").write_text("Just a text file")
 
     return tmp_path
 
@@ -21,8 +22,13 @@ def test_get_files_with_text_without_matches(init_data_folder: Path):
 
     assert result == []
 
-
 def test_get_files_with_text_with_matches(init_data_folder: Path):
     result = get_files_with_text(init_data_folder, "text")
 
     assert set(result) == {init_data_folder / "1.md", init_data_folder / "2.md"}
+
+def test_get_files_with_text_search_only_md_filetypes(init_data_folder: Path):
+    result = get_files_with_text(init_data_folder, "text")
+
+    assert "4.txt" not in set(result) 
+
