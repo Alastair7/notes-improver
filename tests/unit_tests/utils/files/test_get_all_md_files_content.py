07f0cc38764f632e9e67c2de827b93239cbb8180
Alastair7 <alexandru.ban.drmer@gmail.com>
2026-01-29 20:25:06 +0100

    Añadir funcion para obtener el contenido de todas las notas

--- tests/unit_tests/utils/files/test_get_all_md_files_content.py
+++ tests/unit_tests/utils/files/test_get_all_md_files_content.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from textwrap import dedent
 
 import pytest
 
@@ -7,7 +8,21 @@ from utils.files import get_all_md_files_content
 
 @pytest.fixture
 def init_data_folder(tmp_path: Path) -> Path:
+    md_structure = dedent("""\
+    ---
+    keywords: [hola, mundo]
+    title: This is the title in metadata
+    description: One liner description
+    ---
+
+    # Title
+
+    This is a test note
+     """)
+
     _ = (tmp_path / "empty.md").write_text("")
+    _ = (tmp_path / "note_1.md").write_text(md_structure)
+    _ = (tmp_path / "note_2.md").write_text(md_structure)
 
     return tmp_path
 
@@ -15,4 +30,14 @@ def init_data_folder(tmp_path: Path) -> Path:
 def test_get_all_md_files_content_empty_file_is_ignored(init_data_folder: Path):
     notes = get_all_md_files_content(init_data_folder)
 
-    assert len(notes) == 0
+    assert len(notes) == 2
+
+
+def test_get_all_md_files_content_all_files_are_returned_as_list_of_notes(
+    init_data_folder: Path,
+):
+    notes = get_all_md_files_content(init_data_folder)
+
+    assert len(notes) == 2
+    for note in notes:
+        assert note.title == "This is the title in metadata"
